f5c5e7b785d346b9f7cd6b9781144cef3217d41e
DimaP <dmytro_popovych01@gmail.com>
2024-02-18 00:07:04 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# homeW2
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter thrid number: "))
+action = int(input("1 - minimum 2 - maximum 3 - average"))
+
+if action == 1:
+    if num1 < num2 < num3:
+        print(f"minimum: {num1}")
+    elif num2 < num3 < num1:
+        print(f"minimum: {num2}")
+    elif num3 < num2 < num1:
+        print(f"minimum: {num3}")
+elif action == 2:
+    if num1 < num2 < num3:
+        print(f"maximum: {num1}")
+    elif num2 < num3 < num1:
+        print(f"maximum: {num2}")
+    elif num3 < num2 < num1:
+        print(f"maximum: {num3}")
+elif action == 3:
+    print(f"averege: {(num1 + num2 + num3) / 3}")
+else:
+    print("Incorrect input!")
+
+
+
+
+
+
